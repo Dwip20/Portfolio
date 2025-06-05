8cf10e4e839e1cdebe95203193c2293995fcebd8
Dwip Sasmal <112154966+Dwip20@users.noreply.github.com>
2025-06-05 23:38:14 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,6 @@
     if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
     window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
   }
-
   document.addEventListener('scroll', toggleScrolled);
   window.addEventListener('load', toggleScrolled);
 
